e0cb028002a22459c118119a27a9eeb960352356
Bembi98 <iilyaotec322@gmail.com>
2021-12-08 18:34:53 +0300

    Destructure rating

--- src/components/Rating/index.tsx
+++ src/components/Rating/index.tsx
@@ -9,13 +9,14 @@ state={
     stars:this.props.value
 }
     render() {
+    const {stars} = this.state
         return (
             <Box
             >
 
                 <Rating
                     name="simple-controlled"
-                    value={this.state.stars}
+                    value={stars}
                     onChange={(event: any,newValue:any) => {
                         this.setState({stars: Number(newValue)})
                     }}
