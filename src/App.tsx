f13ba7e31b0cf5ea4ae17488ac3b969fa41c5319
Bembi98 <iilyaotec322@gmail.com>
2021-12-15 21:04:16 +0300

    Add header

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import CourseGallery from "./pages/Courses";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import { rootReducer } from "./store";
-
+import Header from "./components/Header";
 const store = createStore(rootReducer);
-class App extends React.Component {
 
+class App extends React.Component {
     render() {
         return(
             <Provider store={store}>
+                <Header/>
         <CourseGallery  />
             </Provider>
     )
